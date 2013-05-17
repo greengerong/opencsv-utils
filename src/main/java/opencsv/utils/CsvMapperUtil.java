19f70ce207f73f10b391654f56f1904bcffc7dee
=greengerong <=grzx2210@163.com>
2013-05-17 22:38:14 +0800

    add utils

---
+++ src/main/java/opencsv/utils/CsvMapperUtil.java
@@ -0,0 +1,39 @@
+package opencsv.utils;
+
+import java.io.Reader;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class CsvMapperUtil {
+
+    private static final Map<String, CsvColumnMapping> columnMap = new HashMap<String, CsvColumnMapping>();
+
+    private CsvMapperUtil() {
+
+    }
+
+    public <T> List<T> fromCsv(Class<T> type, Reader reader) {
+
+        return new CsvMapper<T>(type).withMapping(getColumnMap(type)).fromCsv(reader);
+    }
+
+    private synchronized <T> CsvColumnMapping getColumnMap(Class<T> type) {
+        final String key = type.getName();
+
+        if (!columnMap.containsKey(key)) {
+            columnMap.put(key, new CsvColumnMapping(type));
+        }
+
+        return columnMap.get(key);
+    }
+
+    public <T> void toCsv(Class<T> type, List<T> list, Writer writer) throws Exception {
+        new CsvMapper<T>(type).withMapping(getColumnMap(type)).toCsv(writer, list);
+    }
+
+          public static void Dispose(){
+
+          }
+}
